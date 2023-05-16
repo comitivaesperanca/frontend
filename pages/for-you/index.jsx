471e7c7514260ab0425364075337bddabb62e28a
arthurramires <arthurramires1802@outlook.com>
2023-05-16 14:40:34 -0400

    Feature: adicionando integração com API de predict

--- pages/for-you/index.jsx
+++ pages/for-you/index.jsx
@@ -3,6 +3,9 @@ import { PieChart } from '../../components/PieChart.jsx';
 import BeatLoader from "react-spinners/BeatLoader";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { API_PREDICT_URL } from '@/config/index.js';
+import { formatFinalSentiment, formatFinalSentimentClass } from '@/utils/format.js';
+import Head from 'next/head.js';
 
 const override = {
   display: "block",
@@ -10,12 +13,17 @@ const override = {
   borderColor: "red",
 };
 
-const customers = () => {
+const ForYou = () => {
   const [chartVisibility, setChartVisibility] = useState('hidden');
   const [feelingVisibility, setFeelingVisibility] = useState('hidden');
   const [newsText, setNewsText] = useState('');
-  let [loading, setLoading] = useState(false);
-  let [color, setColor] = useState("#A3D69C");
+  const [loading, setLoading] = useState(false);
+  const [color, setColor] = useState("#A3D69C");
+  const [newsData, setNewsData] = useState('');
+  const [sentiment, setSentiment] = useState('bg-grey-200');
+  const [dailyPositiveSentiment, setDailyPositiveSentiment] = useState(0);
+  const [dailyNeutralSentiment, setDailyNeutralSentiment] = useState(0);
+  const [dailyNegativeSentiment, setDailyNegativeSentiment] = useState(0);
 
   const cleanSearchFields = () => {
     setNewsText('')
@@ -23,21 +31,54 @@ const customers = () => {
     setFeelingVisibility('hidden')
   }
 
+  const setPredictedData = (data) => {
+    setNewsData(data);
+    setDailyPositiveSentiment(data.positive);
+    setDailyNeutralSentiment(data.neutral);
+    setDailyNegativeSentiment(data.negative);
+    console.log(formatFinalSentimentClass(data.final_sentiment));
+    console.log(data.final_sentiment);
+    setSentiment(formatFinalSentimentClass(data.final_sentiment))
+  }
+
+  const getPredictText = (newsText) => {
+        fetch(
+          API_PREDICT_URL + `predict?text_content=${newsText}&model_type=rede_neural`,
+          {
+            method: 'GET',
+          }
+        ).then((res) => res.json())
+        .then((data) => {
+          setLoading(false)
+          setPredictedData(data)
+          toast.success('Sentimento gerado com sucesso!')
+          setChartVisibility('flex')
+          setFeelingVisibility('flex')
+        })
+        .catch((err) => {
+          toast.error('Erro ao gerar sentimento da notícia. Motivo:' + err)
+          setLoading(false)
+        });
+  };
+
   const generateFeeling = async () => {
     if(newsText == ''){
       toast.error("É necessário inserir um texto para gerar sentimento.")
     } else {
       setLoading(true)
-      setTimeout(() => {
-        setLoading(false)
-        setChartVisibility('flex')
-        setFeelingVisibility('flex')
-        setLoading(false)
-      }, 5000)
+      getPredictText(newsText)
     }
   }
 
   return (
+    <>
+    <Head>
+        <title>Radar da Soja - Para Você</title>
+        <meta name='description' content='Radar da Soja' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' href='/soy-logo.svg' />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/datepicker.min.js"></script>
+    </Head>
     <div className='bg-gray-100 min-h-screen'>
       <div className='h-[100px] flex justify-between items-center px-4 pt-4 bg-white-100 w-full border-b-2 border-[#CCCCCC]'>
         <div>
@@ -76,13 +117,13 @@ const customers = () => {
               />
             : <>
               <div className={`lg:w-[300px] sm:w-60 p-4 border rounded-lg bg-white lg:m-0 sm:m-auto ${chartVisibility}`}>
-                <PieChart title={"Sentimentos do texto"} />
+                <PieChart title={"Sentimentos da Notícia"} positive={dailyPositiveSentiment} negative={dailyNegativeSentiment} neutral={dailyNeutralSentiment} />
               </div>
               <div className={`lg:w-[200px] lg:my-0 h-20 p-4 mx-4 border rounded-lg bg-white flex justify-center items-center sm:my-10 ${feelingVisibility}`}>
                 <div className='flex justify-center flex-col'>
-                  <h1 className='text-[#575353] font-bold mb-2'>Sentimento do texto:</h1>
-                  <span className="bg-[#A3D69C] text-[#575353] font-bold px-4 border rounded m-auto">
-                    Positivo
+                  <h1 className='text-[#575353] font-bold mb-2'>Sentimento da notícia:</h1>
+                  <span className={`${sentiment} text-[#575353]  font-bold px-4 border rounded m-auto`} >
+                    { newsData.final_sentiment }
                   </span>
                 </div>
               </div>
@@ -91,7 +132,8 @@ const customers = () => {
         </div>
       </div>
     </div>
+    </>
   );
 };
 
-export default customers;
+export default ForYou;
